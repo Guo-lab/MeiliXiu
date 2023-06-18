c0137a66e46c8b846b887a0c3870d62bc46c6fd9
Guo-lab <74242889+Guo-lab@users.noreply.github.com>
2023-06-19 01:01:17 +0800

    Test Init Completed

--- meilixiu.js
+++ meilixiu.js
@@ -1,5 +1,6 @@
 const express           = require('express')
 const expressHandlebars = require('express-handlebars').engine; // https://stackoverflow.com/questions/71083487/typeerror-expresshandlebars-is-not-a-function
+const handlers          = require("./lib/handlers")
 
 const app  = express()
 app.engine('handlebars', expressHandlebars({
@@ -7,11 +8,9 @@ app.engine('handlebars', expressHandlebars({
 }))
 app.set('view engine', 'handlebars') 
 
-const rand = require('./lib/luckyNumber')
-
-
 // PORT env setting
 const port = process.env.PORT || 3000
+
 
 
 // ------------------------------------ [static middleware should be declared with a top priority]
@@ -21,32 +20,26 @@ app.use(express.static( // ------------ [Middleware: router for each static asse
 
 
 // ------------------------------------ app.METHOD -> HTTP: get, post, ...
-app.get('/', (req, res) => 
-    res.render('home')
-)
-app.get('/about', (req, res) => {
-    res.render('about', {rand: rand.getLuckyNumber()})
-})
-
+app.get('/', handlers.home)
+app.get('/about', handlers.about)
 
 // ------------------------------------ express().use() [Middleware {AFTER router}] to customize 404 and 500   
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.notFound)
+app.use(handlers.serverError)
 
-app.listen(port, () => console.log(
-    "|======================================================|" + "\n" + 
-    "|                                                      |" + "\n" +
-    "|                                                      |" + "\n" +
-    `|---    Express started on http://localhost:${port}.    --|` + "\n" +
-    '|-----         press Ctrl-C to terminate          -----|' + "\n" +
-    "|                                                      |" + "\n" +
-    "|                                                      |" + "\n" +
-    "|======================================================|"
-))
+
+if (require.main == module) {
+    app.listen(port, () => console.log(
+        "|======================================================|" + "\n" + 
+        "|                                                      |" + "\n" +
+        "|                                                      |" + "\n" +
+        `|       Express started on http://localhost:${port}.      |` + "\n" +
+        '|              press Ctrl-C to terminate               |' + "\n" +
+        "|                                                      |" + "\n" +
+        "|                                                      |" + "\n" +
+        "|======================================================|" + "\n"
+    ))
+} else {
+    module.exports = app
+}
+
